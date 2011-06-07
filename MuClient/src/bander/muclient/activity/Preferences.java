bb499377b7539770b9657dae79ff6ff4c59bfd39
banderlabs <banderlabs@b2535ecd-ebef-b94f-8792-9b5bd9a9a8b5>
2011-06-07 22:02:09 +0000

    MuClient: Added Hide Suggestion preference.

--- MuClient/src/bander/muclient/activity/Preferences.java
+++ MuClient/src/bander/muclient/activity/Preferences.java
@@ -1,17 +1,25 @@
 package bander.muclient.activity;
 
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.PreferenceActivity;
 import bander.muclient.R;
 
 /** Preferences activity for MuckClient. */
 public class Preferences extends PreferenceActivity {
+	private static final String KEY_NOSUGGESTIONS = "noSuggestions";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		addPreferencesFromResource(R.xml.preferences);
+		
+		// TYPE_TEXT_FLAG_NO_SUGGESTIONS introduced in Android 2.0, API 5
+		CheckBoxPreference noSuggestionsPreference = (CheckBoxPreference) findPreference(KEY_NOSUGGESTIONS);
+		boolean enabled = (android.os.Build.VERSION.SDK_INT > 4);
+		noSuggestionsPreference.setEnabled(enabled);
+		if (enabled == false) noSuggestionsPreference.setSummaryOff(R.string.pref_noSuggestionsDisabled);
 	}
 
 }
